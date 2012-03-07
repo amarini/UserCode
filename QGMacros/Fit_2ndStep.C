d633b4a9121c627f73c4a1273bae6678e099ead8
amarini <>
2012-03-07 11:35:52 +0000

    improved Fit2ndStep

--- QGMacros/Fit_2ndStep.C
+++ QGMacros/Fit_2ndStep.C
@@ -51,7 +51,7 @@ while (( GetChar(fr) != '[') && (GetChar(fr)!= EOF) )
 	for(int i=0;i<nPar;++i)fscanf(fr,"%f",&par[i]);
 	c='\0';while(c!='\n'){fscanf(fr,"%c",&c);} //go to the end of line
 	//check
-	if(nPar<parameter){perror("I do not have that parameter\n");break;}
+	if(nPar<=parameter){perror("I do not have that parameter\n");break;}
 	//filling the histogram
 	quark->SetBinContent(quark->FindBin((ptmin+ptmax)/2.,(rhomin+rhomax)/2.), par[parameter] );
 }//end of while: loop on the lines
@@ -72,25 +72,43 @@ while (( GetChar(fr) != '[') && (GetChar(fr)!= EOF))
 	for(int i=0;i<nPar;++i)fscanf(fr,"%f",&par[i]);
 	c='\0';while(c!='\n'){fscanf(fr,"%c",&c);} //go to the end of line
 	//check
-	if(nPar<parameter){perror("I do not have that parameter\n");break;}
+	if(nPar<=parameter){perror("I do not have that parameter\n");break;}
 	//filling the histogram
 	gluon->SetBinContent(gluon->FindBin((ptmin+ptmax)/2.,(rhomin+rhomax)/2.), par[parameter] );
 }//end of while: loop on the lines
 fclose(fr);
 
-//END OF READ FILE
-float ChosenPt=90.;
+//END OF READ FILE -- do something
+TCanvas *c1=new TCanvas("c1","c1",1000,1000);
+c1->Divide(4,5);
+for(int PtBin=0;PtBin<nPtBins;++PtBin)
+{
+c1->cd(PtBin+1);
+float ChosenPt=(PtBins[PtBin]+PtBins[PtBin+1])/2;
 TH1D *q=(TH1D*)quark->ProjectionY("_py", quark->GetXaxis()->FindBin( ChosenPt),quark->GetXaxis()->FindBin(ChosenPt ) );
 TH1D *g=(TH1D*)gluon->ProjectionY("_py", gluon->GetXaxis()->FindBin( ChosenPt),gluon->GetXaxis()->FindBin(ChosenPt ) );
-TCanvas *c1=new TCanvas("c1","c1",800,800);
+//TH1D *q=(TH1D*)quark->ProjectionY("_py",PtBin,PtBin );
+//TH1D *g=(TH1D*)gluon->ProjectionY("_py",PtBin,PtBin );
 g->SetMarkerColor(kRed);
 g->SetLineColor(kRed);
 q->SetMarkerStyle(20);
 g->SetMarkerStyle(20);
-q->Draw("AXIS");
-q->Draw("AXIS X+ Y+ SAME");
-q->Draw("P SAME");
-g->Draw("P SAME");
+
+q->SetMinimum(0.9*TMath::Min(q->GetMinimum(),g->GetMinimum()) );
+q->SetMaximum(1.1*TMath::Max(q->GetMaximum(),g->GetMaximum()) );
+
+q->DrawCopy("AXIS");
+q->DrawCopy("AXIS X+ Y+ SAME");
+q->DrawCopy("P SAME");
+g->DrawCopy("P SAME");
+
+TLatex *lat=new TLatex();
+lat->SetNDC();
+lat->SetTextSize(0.08);
+lat->SetTextAlign(23);
+char text[1023];sprintf(text,"P_{T} %.0f - %.0f [GeV]",PtBins[PtBin],PtBins[PtBin+1]);
+lat->DrawLatex(0.5,0.88,text);
+}//loop on the PtBins
 
 }//enf of Fit_2ndStep function
 
