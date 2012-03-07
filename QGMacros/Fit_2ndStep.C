0752710a5442b31885773b6d00bb4185ab92f728
amarini <>
2012-03-07 16:14:23 +0000

    Fit 2nd Level, with TGraph and outFile

--- QGMacros/Fit_2ndStep.C
+++ QGMacros/Fit_2ndStep.C
@@ -12,7 +12,7 @@
 
 char GetChar(FILE *fr);
 
-int Fit_2ndStep(const char fileName[]="nCharged.txt",int parameter=0)
+int Fit_2ndStep(const char fileName[]="nCharged.txt",int parameter=0,const char outFile[]="")
 {
 
 FILE *fr=fopen(fileName,"r");
@@ -24,6 +24,8 @@ char c; //testing character
 //...
 //[gluon]
 //line
+
+
 gROOT->SetStyle("Plain");
  gStyle->SetPalette(1);
  gStyle->SetOptTitle(kFALSE);
@@ -36,6 +38,9 @@ double PtBins[25];int nPtBins=18;
 getBins_int(18,PtBins,20,1000,true);
 PtBins[18]=3500;
 getBins_int(21,RhoBins,0,20,false);
+
+double PtBinsMean[25];for(int i=0;i<nPtBins;++i){PtBinsMean[i]=(PtBins[i]+PtBins[i+1])/2.;}  
+double RhoBinsMean[25];for(int i=0;i<nRhoBins;++i){RhoBinsMean[i]=(RhoBins[i]+RhoBins[i+1])/2.;}  
 //---
 TH2F *quark=new TH2F("quark","quark",nPtBins,PtBins,nRhoBins,RhoBins);
 TH2F *gluon=new TH2F("gluon","gluon",nPtBins,PtBins,nRhoBins,RhoBins);
@@ -87,11 +92,11 @@ fclose(fr);
 TCanvas *c1=new TCanvas("c1","c1",1000,1000);
 c1->Divide(4,5);
 //this histograms will contain the fit results y=ax + b
-TH1F *aq=new TH1F("aq","aq",nPtBins,PtBins);
-TH1F *bq=new TH1F("bq","bq",nPtBins,PtBins);
-TH1F *ag=new TH1F("ag","ag",nPtBins,PtBins);
-TH1F *bg=new TH1F("bg","bg",nPtBins,PtBins);
-TF1 *line=new TF1("line","[1]*x + [0]",0,20);
+TGraphErrors *aq=new TGraphErrors();
+TGraphErrors *bq=new TGraphErrors();
+TGraphErrors *ag=new TGraphErrors();
+TGraphErrors *bg=new TGraphErrors();int graphCount=0;
+TF1 *line=new TF1("line","[1]*x + [0]",3,14);
 
 for(int PtBin=0;PtBin<nPtBins;++PtBin)
 {
@@ -99,23 +104,30 @@ c1->cd(PtBin+1);
 float ChosenPt=(PtBins[PtBin]+PtBins[PtBin+1])/2;
 char name[1023];
 sprintf(name,"_qpy%.0f",PtBins[PtBin]);
-TH1D *q=(TH1D*)quark->ProjectionY(name, quark->GetXaxis()->FindBin( ChosenPt),quark->GetXaxis()->FindBin(ChosenPt ) );
+TH1D *q2=(TH1D*)quark->ProjectionY(name, quark->GetXaxis()->FindBin( ChosenPt),quark->GetXaxis()->FindBin(ChosenPt ) );
 sprintf(name,"_gpy%.0f",PtBins[PtBin]);
-TH1D *g=(TH1D*)gluon->ProjectionY(name, gluon->GetXaxis()->FindBin( ChosenPt),gluon->GetXaxis()->FindBin(ChosenPt ) );
-//TH1D *q=(TH1D*)quark->ProjectionY("_py",PtBin,PtBin );
-//TH1D *g=(TH1D*)gluon->ProjectionY("_py",PtBin,PtBin );
+TH1D *g2=(TH1D*)gluon->ProjectionY(name, gluon->GetXaxis()->FindBin( ChosenPt),gluon->GetXaxis()->FindBin(ChosenPt ) );
+TGraphErrors *q=new TGraphErrors();
+TGraphErrors *g=new TGraphErrors();
+int k=0;
+for(int j=0; j<q2->GetNbinsX();++j) {
+					if((q2->GetBinContent(j)==0)||(g2->GetBinContent(j)==0))continue;
+					q->SetPoint(k,RhoBinsMean[j],q2->GetBinContent(j));
+					g->SetPoint(k,RhoBinsMean[j],g2->GetBinContent(j));
+					k++;
+					}
+
+
 g->SetMarkerColor(kRed);
 g->SetLineColor(kRed);
 q->SetMarkerStyle(20);
 g->SetMarkerStyle(20);
 
-q->SetMinimum(0.9*TMath::Min(q->GetMinimum(),g->GetMinimum()) );
-q->SetMaximum(1.1*TMath::Max(q->GetMaximum(),g->GetMaximum()) );
+q->SetMinimum(0.9*TMath::Min(q2->GetMinimum(),g2->GetMinimum()) );
+q->SetMaximum(1.1*TMath::Max(q2->GetMaximum(),g2->GetMaximum()) );
 
-q->DrawCopy("AXIS");
-q->DrawCopy("AXIS X+ Y+ SAME");
-q->DrawCopy("P SAME");
-g->DrawCopy("P SAME");
+q->Draw("AP");
+g->Draw("P SAME");
 
 TLatex *lat=new TLatex();
 lat->SetNDC();
@@ -125,17 +137,25 @@ char text[1023];sprintf(text,"P_{T} %.0f - %.0f [GeV]",PtBins[PtBin],PtBins[PtBi
 lat->DrawLatex(0.5,0.88,text);
 
 //I want to fit with lines and same the parameters in to histos (vs Pt)
-q->Fit("line","QN");
-q->Fit("line","QNM");
+q->Fit("line","RQN");
+q->Fit("line","RQNM");
 line->SetLineColor(kBlack);line->DrawCopy("SAME");
-aq->SetBinContent(aq->FindBin( (PtBins[PtBin]+PtBins[PtBin+1])/2 ),line->GetParameter(1));
-bq->SetBinContent(bq->FindBin( (PtBins[PtBin]+PtBins[PtBin+1])/2 ),line->GetParameter(0));
-g->Fit("line","QN");
-g->Fit("line","QNM");
+//aq->SetBinContent(aq->FindBin( (PtBins[PtBin]+PtBins[PtBin+1])/2 ),line->GetParameter(1));
+//bq->SetBinContent(bq->FindBin( (PtBins[PtBin]+PtBins[PtBin+1])/2 ),line->GetParameter(0));
+aq->SetPoint(graphCount,PtBinsMean[PtBin],line->GetParameter(1));
+bq->SetPoint(graphCount,PtBinsMean[PtBin],line->GetParameter(0));
+aq->SetPointError(graphCount,0,line->GetParError(1));
+bq->SetPointError(graphCount,0,line->GetParError(0));
+g->Fit("line","RQN");
+g->Fit("line","RQNM");
 line->SetLineColor(kRed);line->DrawCopy("SAME");
-ag->SetBinContent(ag->FindBin( (PtBins[PtBin]+PtBins[PtBin+1])/2 ),line->GetParameter(1));
-bg->SetBinContent(bg->FindBin( (PtBins[PtBin]+PtBins[PtBin+1])/2 ),line->GetParameter(0));
-
+//ag->SetBinContent(ag->FindBin( (PtBins[PtBin]+PtBins[PtBin+1])/2 ),line->GetParameter(1));
+//bg->SetBinContent(bg->FindBin( (PtBins[PtBin]+PtBins[PtBin+1])/2 ),line->GetParameter(0));
+ag->SetPoint(graphCount,PtBinsMean[PtBin],line->GetParameter(1));
+bg->SetPoint(graphCount,PtBinsMean[PtBin],line->GetParameter(0));
+ag->SetPointError(graphCount,0,line->GetParError(1));
+bg->SetPointError(graphCount,0,line->GetParError(0));
+graphCount++;
 
 }//loop on the PtBins
 
@@ -145,18 +165,36 @@ ag->SetMarkerStyle(20);
 bg->SetMarkerStyle(20);
 ag->SetMarkerColor(kRed);
 bg->SetMarkerColor(kRed);
+TF1 *pol3=new TF1("pol3","[0]+[1]*TMath::Log(x)+[2]*TMath::Log(x)**2+[3]*TMath::Log(x)**3",20,3500);//range in pt
+TF1 *pol1=new TF1("pol1","[0]+[1]*TMath::Log(x)",20,3500);//range in pt
+
+FILE *fw;
+if(outFile[0]!='\0'){
+		fw=fopen(outFile,"a");
+		fprintf(stderr,"Opening %s in append mode\n",outFile);
+		}
 
 TPad *Pad;
 c1->cd(nPtBins+1);sprintf(name,"c1_%d",nPtBins+1);Pad=(TPad*)c1->FindObject(name);Pad->SetLogx();
-aq->SetMaximum(1.1*TMath::Max(aq->GetMaximum(),ag->GetMaximum()));
-aq->SetMinimum(0.9*TMath::Min(aq->GetMinimum(),ag->GetMinimum()));
-aq->Draw("P");
+aq->Draw("AP");
 ag->Draw("P SAME");
+aq->Fit("pol1","NQ");aq->Fit("pol1","NQM");
+pol1->SetLineColor(kBlack);pol1->DrawCopy("SAME");
+if(outFile[0]!='\0')fprintf(fw,"%d aq %f %f \n",parameter,pol1->GetParameter(0),pol1->GetParameter(1));
+ag->Fit("pol1","NQ");ag->Fit("pol1","NQM");
+pol1->SetLineColor(kRed);pol1->DrawCopy("SAME");
+if(outFile[0]!='\0')fprintf(fw,"%d ag %f %f \n",parameter,pol1->GetParameter(0),pol1->GetParameter(1));
 c1->cd(nPtBins+2);sprintf(name,"c1_%d",nPtBins+2);Pad=(TPad*)c1->FindObject(name);Pad->SetLogx();
-bq->SetMaximum(1.1*TMath::Max(bq->GetMaximum(),bg->GetMaximum()));
-bq->SetMinimum(0.9*TMath::Min(bq->GetMinimum(),bg->GetMinimum()));
-bq->Draw("P");
+bq->Draw("AP");
 bg->Draw("P SAME");
+bq->Fit("pol3","NQ");bq->Fit("pol3","NQM");
+pol3->SetLineColor(kBlack);pol3->DrawCopy("SAME");
+if(outFile[0]!='\0')fprintf(fw,"%d bq %f %f %f %f\n",parameter,pol3->GetParameter(0),pol3->GetParameter(1),pol3->GetParameter(2),pol3->GetParameter(3));
+bg->Fit("pol3","NQ");bg->Fit("pol3","NQM");
+pol3->SetLineColor(kRed);pol3->DrawCopy("SAME");
+if(outFile[0]!='\0')fprintf(fw,"%d bg %f %f %f %f\n",parameter,pol3->GetParameter(0),pol3->GetParameter(1),pol3->GetParameter(2),pol3->GetParameter(3));
+
+
 
 }//enf of Fit_2ndStep function
 
