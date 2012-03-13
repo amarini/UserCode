3cdfb8e383a127126d7497ddbce325941b7c1631
amarini <>
2012-03-13 17:14:37 +0000

    *** empty log message ***

--- QGMacros/PlotUnfoldedParams.C
+++ QGMacros/PlotUnfoldedParams.C
@@ -7,6 +7,11 @@ inline double gammadistr_(double* x, double* par)
 {
         return TMath::Exp( - x[0] *par[0]/par[1] ) * TMath::Power(x[0],par[0]-1) * TMath::Power(par[1]/par[0],-par[0])/TMath::Gamma(par[0]) ;
 }
+inline double functionPtD_(double * x ,double*par)
+{
+        if((x[0]-par[0])<0)return 0;
+        return TMath::Exp( - (x[0]-par[0]) *par[1]/par[2] ) * TMath::Power((x[0]-par[0]),par[1]-1) * TMath::Power(par[2]/par[1],-par[1])/TMath::Gamma(par[1]) ;
+}
 int PlotUnfoldedParams(const char *varName="nCharged",const char*Directory="Inversion",int PtMin=80,int PtMax=100)
 {
 TFile *f;
@@ -22,19 +27,14 @@ TCanvas *c0=new TCanvas("c0","c0",800,800);
 c0->Divide(3,3);
 for(int i=0; i<13; i+=2)
 	{
-	//sprintf(fileName,"../Svd/nCharged_pt101_127_rho%d_%d.root",i,i+2);
-	//sprintf(fileName,"../Svd/nCharged_pt80_100_rho%d_%d.root",i,i+2);
-//	sprintf(fileName,"../Inversion/nCharged_pt80_100_rho%d_%d.root",i,i+2);
-//	sprintf(fileName,"../Inversion/nNeutral_pt80_100_rho%d_%d.root",i,i+2);
-//	sprintf(fileName,"../Inversion/ptD_pt80_100_rho%d_%d.root",i,i+2);
 	sprintf(fileName,"../%s/%s_pt%d_%d_rho%d_%d.root",Directory,varName,PtMin,PtMax,i,i+2);
-	//sprintf(fileName,"../Inversion/nCharged_pt101_127_rho%d_%d.root",i,i+2);
 	f=TFile::Open(fileName);
 	c=(TCanvas*)f->Get("c1_n3");
 	q=(TH1F*)c->FindObject("Unfold_1");
+	q->Scale(1./q->Integral("width"));
 	c0->cd(i/2+1);
 	q->Draw("P");
-	TGraphErrors *Q=new TGraphErrors();int a=0;for(int k=0;k<=q->GetNbinsX();k++)if(q->GetBinContent(k)>0){Q->SetPoint(a,q->GetBinCenter(k),q->GetBinContent(k));Q->SetPointError(a,1,q->GetBinError(k));a++;}
+	TGraphErrors *Q=new TGraphErrors();int a=0;for(int k=0;k<=q->GetNbinsX();k++)if(q->GetBinError(k)>0.001){Q->SetPoint(a,q->GetBinCenter(k),q->GetBinContent(k));Q->SetPointError(a,1,q->GetBinError(k));a++;}
 		gammadistr->SetParameter(1,q->GetMean());
         	gammadistr->SetParameter(0,q->GetMean()*q->GetMean()/(q->GetRMS()*q->GetRMS()));
         	Q->Fit("gamma","N Q");//N=Don't Draw
@@ -49,9 +49,10 @@ for(int i=0; i<13; i+=2)
 		rq1->SetBinContent(rq1->FindBin(i+1),gammadistr->GetParameter(1));
 		rq1->SetBinError  (rq1->FindBin(i+1),gammadistr->GetParError(1));
 	g=(TH1F*)c->FindObject("Unfold_2");
+	g->Scale(1./g->Integral("width"));
 	c0->cd(i/2+1);
 	g->Draw("P SAME");
-	TGraphErrors *G=new TGraphErrors();int a=0;for(int k=0;k<=g->GetNbinsX();k++)if(g->GetBinContent(k)>0){G->SetPoint(a,g->GetBinCenter(k),g->GetBinContent(k));G->SetPointError(a,1,g->GetBinError(k));a++;}
+	TGraphErrors *G=new TGraphErrors();int a=0;for(int k=0;k<=g->GetNbinsX();k++)if(g->GetBinError(k)>0.001){G->SetPoint(a,g->GetBinCenter(k),g->GetBinContent(k));G->SetPointError(a,1,g->GetBinError(k));a++;}
 		gammadistr->SetParameter(1,g->GetMean());
         	gammadistr->SetParameter(0,g->GetMean()*g->GetMean()/(g->GetRMS()*g->GetRMS()));
         	G->Fit("gamma","N Q");//N=Don't Draw
@@ -78,13 +79,19 @@ rq1->SetMarkerStyle(20);
 rg0->SetMarkerStyle(20);
 rg1->SetMarkerStyle(20);
 
+TF1* pol1=new TF1("pol1","[0] +[1]*x",0,20);
 TCanvas *c1=new TCanvas();
 c1->Divide(2);
 c1->cd(1);
 rq0->Draw("P");
 rg0->Draw("P SAME");
+rq0->Fit("pol1","N Q");pol1->SetLineColor(kBlack);pol1->DrawCopy("SAME");
+rg0->Fit("pol1","N Q");pol1->SetLineColor(kRed);pol1->DrawCopy("SAME");
+
 c1->cd(2);
 rq1->Draw("P");
 rg1->Draw("P SAME");
+rq1->Fit("pol1","N Q");pol1->SetLineColor(kBlack);pol1->DrawCopy("SAME");
+rg1->Fit("pol1","N Q");pol1->SetLineColor(kRed);pol1->DrawCopy("SAME");
 
 }
