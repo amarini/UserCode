900f52c1677d18aebf3a954ef1a99617e760c204
amarini <>
2012-03-21 07:09:44 +0000

    ComputeMixture.C

--- QGMacros/new/ComputeMixture.C
+++ QGMacros/new/ComputeMixture.C
@@ -35,26 +35,38 @@ int ComputeMixture(const char*fileName1,
 	char selection[1023];
 	float q,g,o;
 	float eq,eg,eo;
-	sprintf(selection,"eventWeight*( %f < ptJet0 && ptJet0<%f && %f<rhoPF && rhoPF<%f && abs(pdgIdJet0)<=4)",PtMin,PtMax,RhoMin,RhoMax); //dijet - file 1
+	sprintf(selection,"eventWeight*( %f < ptJet0 && ptJet0<%f && %f<rhoPF && rhoPF<%f && abs(pdgIdJet0)<=4 && pdgIdJet0!=0)",PtMin,PtMax,RhoMin,RhoMax); //dijet - file 1
 
 	TH1F *hq=new TH1F("quark1","quark1",100,0,100);
 		hq->Sumw2();
 		t1->Draw("abs(pdgIdJet0)>>quark1",selection,"goff");// I'm interested only in the integral
-		q=hq->IntegralAndError(1,100,eq);
+		q=hq->Integral();
+		eq=TMath::Sqrt(
+				TMath::Power(hq->GetBinError( hq->FindBin(1)),2) +
+				TMath::Power(hq->GetBinError( hq->FindBin(2)),2) +
+				TMath::Power(hq->GetBinError( hq->FindBin(3)),2) +
+				TMath::Power(hq->GetBinError( hq->FindBin(4)),2) 
+				);
 		fprintf(stderr,"selection quark ==%s==\nq=%f\n",selection,q);
 
 	sprintf(selection,"eventWeight*( %f < ptJet0 && ptJet0<%f && %f<rhoPF && rhoPF<%f && abs(pdgIdJet0)==21)",PtMin,PtMax,RhoMin,RhoMax); //gluon
 	TH1F *hg=new TH1F("gluon1","gluon1",100,0,100);
 		hg->Sumw2();
 		t1->Draw("abs(pdgIdJet0)>>gluon1",selection,"goff");
-		g=hg->IntegralAndError(1,100,eg);
+		g=hg->Integral();
+		eg=hg->GetBinError( hg->FindBin(21));
 		fprintf(stderr,"selection gluon ==%s==\ng=%f\n",selection,g);
 
-	sprintf(selection,"eventWeight*( %f < ptJet0 && ptJet0<%f && %f<rhoPF && rhoPF<%f && abs(pdgIdJet0)!=21 && abs(pdgIdJet0)>4 )",PtMin,PtMax,RhoMin,RhoMax); //gluon
+	sprintf(selection,"eventWeight*( %f < ptJet0 && ptJet0<%f && %f<rhoPF && rhoPF<%f && ((abs(pdgIdJet0)!=21 && abs(pdgIdJet0)>4)||pdgIdJet0==0) )",PtMin,PtMax,RhoMin,RhoMax); //gluon
 	TH1F *ho=new TH1F("other1","other1",100,0,100);
 		ho->Sumw2();
 		t1->Draw("abs(pdgIdJet0)>>other1",selection,"goff");
-		o=ho->IntegralAndError(1,100,eo);
+		o=ho->Integral();
+		eo=TMath::Sqrt(
+				TMath::Power(hq->GetBinError( hq->FindBin(5)),2) +
+				TMath::Power(hq->GetBinError( hq->FindBin(99)),2) +
+				TMath::Power(hq->GetBinError( hq->FindBin(100)),2) 
+				);
 		fprintf(stderr,"selection others ==%s==\no=%f\n",selection,o);
 	
 	fprintf(stderr,"DIJET q/q+g=%.3f q/q+g+o=%.3f o/q+g+o=%.3f\n",q/(q+g),q/(q+g+o),o/(q+g+o));
