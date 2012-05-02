bec21c2b51e9646cb97c8973f66049de0b507ba8
amarini <>
2012-05-02 13:42:20 +0000

    Not refitting again

--- QGMacros/PlotUnfoldedParams.C
+++ QGMacros/PlotUnfoldedParams.C
@@ -6,7 +6,7 @@
 //Fit procedure with points exclusion
 #include "new/FitN.C"
 
-inline double gammadistr_(double* x, double* par)
+inline double func_(double* x, double* par)
 {
         return TMath::Exp( - x[0] *par[0]/par[1] ) * TMath::Power(x[0],par[0]-1) * TMath::Power(par[1]/par[0],-par[0])/TMath::Gamma(par[0]) ;
 }
@@ -15,60 +15,36 @@ inline double functionPtD_(double * x ,double*par)
         if((x[0]-par[0])<0)return 0;
         return TMath::Exp( - (x[0]-par[0]) *par[1]/par[2] ) * TMath::Power((x[0]-par[0]),par[1]-1) * TMath::Power(par[2]/par[1],-par[1])/TMath::Gamma(par[1]) ;
 }
-int PlotUnfoldedParams(const char *varName="nCharged",const char*Directory="Inversion",int PtMin=80,int PtMax=100)
+int PlotUnfoldedParams(const char *varName="nCharged",const char*Directory="Inversion",int PtMin=80,int PtMax=100,const char *outTxtFileName="")
 {
 TFile *f;
 TCanvas *c;
-TH1F*q,*g;
 TH1F*rq0=new TH1F("rq0","rq0",10,0,20);
 TH1F*rg0=new TH1F("rg0","rg0",10,0,20);
 TH1F*rq1=new TH1F("rq1","rq1",10,0,20);
 TH1F*rg1=new TH1F("rg1","rg1",10,0,20);
 char fileName[1023];
-TF1 *gammadistr=new TF1("gamma",gammadistr_,0,100,2);
-TCanvas *c0=new TCanvas("c0","c0",800,800);
-c0->Divide(3,3);
-for(int i=0; i<13; i+=2)
+TF1 *func;//new TF1("gamma",func_,0,100,2);
+for(int i=0; i<20; i+=2)
 	{
-	sprintf(fileName,"../%s/%s_pt%d_%d_rho%d_%d.root",Directory,varName,PtMin,PtMax,i,i+2);
+	sprintf(fileName,"../%s/%s_pt%d_%d_rho%d_%d_UN.root",Directory,varName,PtMin,PtMax,i,i+2);
 	f=TFile::Open(fileName);
-	c=(TCanvas*)f->Get("c1_n3");
-	q=(TH1F*)c->FindObject("Unfold_1");
-	q->Scale(1./q->Integral("width"));
-	c0->cd(i/2+1);
-	q->Draw("P");
-	TGraphErrors *Q=new TGraphErrors();int a=0;for(int k=0;k<=q->GetNbinsX();k++)if(q->GetBinError(k)>0.001){Q->SetPoint(a,q->GetBinCenter(k),q->GetBinContent(k));Q->SetPointError(a,1,q->GetBinError(k));a++;}
-		gammadistr->SetParameter(1,q->GetMean());
-        	gammadistr->SetParameter(0,q->GetMean()*q->GetMean()/(q->GetRMS()*q->GetRMS()));
-        	Q->Fit("gamma","N Q");//N=Don't Draw
-        	Q->Fit("gamma","N M Q");//N=Don't Draw M=More
-		//Q->Draw("* SAME");
-        	gammadistr->SetLineColor(kBlack);
-        	gammadistr->SetName("gamma_quark");
-        	gammadistr->DrawCopy("SAME");
-        	gammadistr->SetName("gamma");
-		rq0->SetBinContent(rq0->FindBin(i+1),gammadistr->GetParameter(0));
-		rq0->SetBinError  (rq0->FindBin(i+1),gammadistr->GetParError(0));
-		rq1->SetBinContent(rq1->FindBin(i+1),gammadistr->GetParameter(1));
-		rq1->SetBinError  (rq1->FindBin(i+1),gammadistr->GetParError(1));
-	g=(TH1F*)c->FindObject("Unfold_2");
-	g->Scale(1./g->Integral("width"));
-	c0->cd(i/2+1);
-	g->Draw("P SAME");
-	TGraphErrors *G=new TGraphErrors();int a=0;for(int k=0;k<=g->GetNbinsX();k++)if(g->GetBinError(k)>0.001){G->SetPoint(a,g->GetBinCenter(k),g->GetBinContent(k));G->SetPointError(a,1,g->GetBinError(k));a++;}
-		gammadistr->SetParameter(1,g->GetMean());
-        	gammadistr->SetParameter(0,g->GetMean()*g->GetMean()/(g->GetRMS()*g->GetRMS()));
-        	G->Fit("gamma","N Q");//N=Don't Draw
-        	G->Fit("gamma","N M  Q");//N=Don't Draw M=More
-		//G->Draw("* SAME");
-        	gammadistr->SetLineColor(kRed);
-        	gammadistr->SetName("gamma_gluon");
-        	gammadistr->DrawCopy("SAME");
-        	gammadistr->SetName("gamma");
-		rg0->SetBinContent(rg0->FindBin(i+1),gammadistr->GetParameter(0));
-		rg0->SetBinError  (rg0->FindBin(i+1),gammadistr->GetParError(0));
-		rg1->SetBinContent(rg1->FindBin(i+1),gammadistr->GetParameter(1));
-		rg1->SetBinError  (rg1->FindBin(i+1),gammadistr->GetParError(1));
+	if(f==NULL){printf("File %s does not exist. Skipped.\n",fileName);continue;}
+	c=(TCanvas*)f->Get("c2");
+	if(varName[0]=='n')func=c->FindObject("gamma_quark");
+	else 	func=c->FindObject("functionPtD_quark");	
+
+		rq0->SetBinContent(rq0->FindBin(i+1),func->GetParameter(0));
+		rq0->SetBinError  (rq0->FindBin(i+1),func->GetParError(0));
+		rq1->SetBinContent(rq1->FindBin(i+1),func->GetParameter(1));
+		rq1->SetBinError  (rq1->FindBin(i+1),func->GetParError(1));
+
+	if(varName[0]=='n') func=c->FindObject("gamma_gluon");
+	else 	func=c->FindObject("functionPtD_gluon");	
+		rg0->SetBinContent(rg0->FindBin(i+1),func->GetParameter(0));
+		rg0->SetBinError  (rg0->FindBin(i+1),func->GetParError(0));
+		rg1->SetBinContent(rg1->FindBin(i+1),func->GetParameter(1));
+		rg1->SetBinError  (rg1->FindBin(i+1),func->GetParError(1));
 	//ora li fitto
 	f->Close();
 	}
@@ -97,12 +73,15 @@ rg0->Draw("P SAME");
 		B->SetRange(0,8);
 		B->SetTF1(pol0);
 B->Fit();
+FILE *fw;
+if(outTxtFileName[0]!=0)fw=fopen(outTxtFileName,"a");
+else fw=stdout;
 //rq0->Fit("pol1","N Q");
-pol0->SetLineColor(kBlack);pol0->DrawCopy("SAME");printf("==0 %f bq %f==\n==0 %f aq 0==\n",TMath::Log((PtMin+PtMax)/2.),pol0->GetParameter(0),TMath::Log((PtMin+PtMax)/2.));
+pol0->SetLineColor(kBlack);pol0->DrawCopy("SAME");fprintf(fw,"==0 %f bq %f==\n==0 %f aq 0==\n",TMath::Log((PtMin+PtMax)/2.),pol0->GetParameter(0),TMath::Log((PtMin+PtMax)/2.));
 //rg0->Fit("pol1","N Q");
 B->SetTH1F(rg0);
 B->Fit();
-pol0->SetLineColor(kRed);pol0->DrawCopy("SAME");printf("==0 %f bg %f==\n==0 %f ag 0==\n",TMath::Log((PtMin+PtMax)/2.),pol0->GetParameter(0),TMath::Log((PtMin+PtMax)/2.));
+pol0->SetLineColor(kRed);pol0->DrawCopy("SAME");fprintf(fw,"==0 %f bg %f==\n==0 %f ag 0==\n",TMath::Log((PtMin+PtMax)/2.),pol0->GetParameter(0),TMath::Log((PtMin+PtMax)/2.));
 
 		B->SetRange(0,12);
 		B->SetTF1(pol1);
@@ -112,10 +91,10 @@ rg1->Draw("P SAME");
 B->SetTH1F(rq1);
 B->Fit();
 //rq1->Fit("pol1","N Q");
-pol1->SetLineColor(kBlack);pol1->DrawCopy("SAME");printf("==1 %f bq %f==\n==1 %f aq %f==\n",TMath::Log((PtMin+PtMax)/2.),pol1->GetParameter(0),TMath::Log((PtMin+PtMax)/2.),pol1->GetParameter(1));
+pol1->SetLineColor(kBlack);pol1->DrawCopy("SAME");fprintf(fw,"==1 %f bq %f==\n==1 %f aq %f==\n",TMath::Log((PtMin+PtMax)/2.),pol1->GetParameter(0),TMath::Log((PtMin+PtMax)/2.),pol1->GetParameter(1));
 B->SetTH1F(rg1);
 B->Fit();
 //rg1->Fit("pol1","N Q");
-pol1->SetLineColor(kRed);pol1->DrawCopy("SAME");printf("==1 %f bg %f==\n==1 %f ag %f==\n",TMath::Log((PtMin+PtMax)/2.),pol1->GetParameter(0),TMath::Log((PtMin+PtMax)/2.),pol1->GetParameter(1));
-
+pol1->SetLineColor(kRed);pol1->DrawCopy("SAME");fprintf(fw,"==1 %f bg %f==\n==1 %f ag %f==\n",TMath::Log((PtMin+PtMax)/2.),pol1->GetParameter(0),TMath::Log((PtMin+PtMax)/2.),pol1->GetParameter(1));
+return 0;
 }
