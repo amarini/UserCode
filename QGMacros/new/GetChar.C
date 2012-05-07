bf5df5e8daf096dbbc546335cf56fbaac13eaf21
amarini <>
2012-05-07 13:16:41 +0000

    Added GetChar

---
+++ QGMacros/new/GetChar.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+char GetChar(FILE *fr)
+{
+fpos_t pos;//position in the file
+char c;
+//get position of the line to be analized;
+fgetpos(fr,&pos);
+c=fgetc(fr);
+fsetpos(fr,&pos); //moving back to the beginning of the line
+return c;
+}
