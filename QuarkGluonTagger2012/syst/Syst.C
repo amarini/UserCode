114b0700aaa7efd30411e3f01b5983c4be319b04
amarini <>
2013-03-12 10:34:53 +0000

    add Histo Syst

--- QuarkGluonTagger2012/syst/Syst.C
+++ QuarkGluonTagger2012/syst/Syst.C
@@ -17,8 +17,11 @@ int RhoBin=-999;
 int EtaBin=-999;
 int tag=-999;
 
-if(string(tagger)==string("QGL")) tag=1;
-if(string(tagger)==string("MLP")) tag=2;
+if(string(tagger)==string("QGLFit")) 	tag=1;
+if(string(tagger)==string("QGLMLP")) 	tag=2;
+if(string(tagger)==string("MLP")) 	tag=2;
+if(string(tagger)==string("QGLHisto")) 	tag=3;
+if(string(tagger)==string("QGL")) 	tag=3;
 
 if( rho <=15 ) RhoBin=1;
 if( rho >15  ) RhoBin=2;
@@ -69,6 +72,22 @@ case 2212: a= 1.10; b= 0.240;  break;
 case 2222: a= .92; b= 0.2;  break;
 case 2213: a= 1.00; b= 0.04;  break;
 case 2223: a= 1.06; b= 0.1;  break;
+
+//HISTO
+case 3111: a=0.90; b=0.08; break;
+case 3121: a=0.935; b=0.0; break;
+case 3112: a=0.905; b=-0.80; break;
+case 3122: a=0.785; b=-0.18; break;
+case 3113: a=0.68; b=0.05; break;
+case 3123: a=0.825; b=0.01; break;
+//FWD
+case 3211: a=0.89; b=-0.39; break;
+case 3221: a=0.695; b=-0.66; break;
+case 3212: a=1.245; b=-0.74; break;
+case 3222: a=0.82; b=-.310; break;
+case 3213: a=.99; b=.09; break;
+case 3223: a=1.005; b=-0.04; break;
+
 default: return -1;
 }
 
